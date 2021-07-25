cc5777aa41968c8510d313d3838fd65c3693be0b
Kim Jose <kim.murakawa@outlook.com>
2021-07-25 19:27:46 -0300

    chore: delete src/invalid_tr.ts

--- src/invalid_tr.ts
+++
@@ -1,6 +0,0 @@
-export class InvalidTriangle extends Error {
-  constructor(message: string){
-      super(message)
-      this.name = 'Invalid Triangle'
-  }
-}
--- src/negative_tr.ts
+++
@@ -1,6 +0,0 @@
-export class NegativeTriangle extends Error {
-    constructor(message: string){
-        super(message)
-        this.name = 'Invalid Triangle - Negative Side'
-    }
-  }
