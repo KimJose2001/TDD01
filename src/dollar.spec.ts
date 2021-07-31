229336babbd546fb202a15663c6d38d16d25b443
Kim Jose <kim.murakawa@outlook.com>
2021-07-31 16:57:49 -0300

    test: base euro class

--- src/dollar.spec.ts
+++ src/dollar.spec.ts
@@ -7,7 +7,11 @@ describe('Dollar', () => {
     expect(five.times(3).equals(new Dollar(15))).toBeTruthy()
     })
 
-
+    it('should return 10 when five euros are multiplied by two and 15 when multiplied by three', () =>{
+        const five: Euro = new Euro(5)
+        expect(five.times(2).equals(new Euro(10))).toBeTruthy()
+        expect(five.times(3).equals(new Euro(15))).toBeTruthy()
+        })
 
     it('should return true when dollars of the same value are compared', () =>{
         expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy()
